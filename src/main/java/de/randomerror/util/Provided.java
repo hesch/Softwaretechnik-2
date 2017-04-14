751ec9fe0690353b6f07950eaa118de2dbfb3f21
Henri Schmidt <henri@reschmi.de>
2017-04-14 22:25:22 +0200

    add marking annotation for injector

---
+++ src/main/java/de/randomerror/util/Provided.java
@@ -0,0 +1,11 @@
+package de.randomerror.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Henri on 12.04.17.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Provided {
+}
