f29c16ddec3c0af9b41326cedbf5d438dde38c91
Henri Schmidt <henri@reschmi.de>
2017-05-08 21:22:15 +0200

    add AllArgsConstructor to Entities

--- src/main/java/de/randomerror/entity/Delivery.java
+++ src/main/java/de/randomerror/entity/Delivery.java
@@ -1,5 +1,6 @@
 package de.randomerror.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
  * Created by henri on 08.05.17.
  */
 @Data
+@AllArgsConstructor
 public class Delivery {
     private List<DeliveryItem> items;
     private DeliveryHuman human;
