0952cf31df0c496c647c52cdaebd46c1e6f6cc5f
Henri Schmidt <henri@reschmi.de>
2017-05-08 21:23:05 +0200

    restructure Main to allow for testData

--- src/main/java/de/randomerror/Main.java
+++ src/main/java/de/randomerror/Main.java
@@ -1,6 +1,11 @@
 package de.randomerror;
 
 import de.randomerror.GUI.view.LoginView;
+import de.randomerror.entity.Delivery;
+import de.randomerror.persistence.DeliveryHumanRepo;
+import de.randomerror.persistence.DeliveryRepo;
+import de.randomerror.persistence.ProductClassRepo;
+import de.randomerror.persistence.ProductRepo;
 import de.randomerror.util.Injector;
 import de.randomerror.util.Provided;
 
@@ -10,9 +15,23 @@ import java.io.IOException;
  * Created by Henri on 11.04.17.
  */
 public class Main {
+    DeliveryHumanRepo deliveryHumanRepo;
+    DeliveryRepo deliveryRepo;
+    ProductRepo productRepo;
+    ProductClassRepo productClassRepo;
+    LoginView view;
 
     public static void main(String[] args) throws IOException {
         Injector.init();
-        Injector.getProvided(LoginView.class).show();
+        Injector.getProvided(Main.class).init();
+    }
+
+    public void init() {
+        generateTestData();
+        view.show();
+    }
+
+    public void generateTestData() {
+
     }
 }
