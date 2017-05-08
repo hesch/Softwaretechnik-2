f3785567d2b2838936ff2d5ab27865416415b4dd
Henri Schmidt <henri@reschmi.de>
2017-05-08 20:01:33 +0200

    Main now calls correct View from Injector classPool

--- src/main/java/de/randomerror/Main.java
+++ src/main/java/de/randomerror/Main.java
@@ -1,7 +1,18 @@
 package de.randomerror;
 
+import de.randomerror.GUI.view.LoginView;
+import de.randomerror.util.Injector;
+import de.randomerror.util.Provided;
+
+import java.io.IOException;
+
 /**
  * Created by Henri on 11.04.17.
  */
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Injector.init();
+        Injector.getProvided(LoginView.class).show();
+    }
 }
