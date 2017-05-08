7d07a24f1970f5f4f77d6b16ed047c802f6588d0
jwennrich <janwe1508@gmail.com>
2017-05-08 22:24:32 +0200

    add SalesView

--- src/main/java/de/randomerror/GUI/view/SalesView.java
+++ src/main/java/de/randomerror/GUI/view/SalesView.java
@@ -2,11 +2,40 @@ package de.randomerror.GUI.view;
 
 import de.randomerror.util.Provided;
 
+import javax.swing.*;
+
 /**
  * Created by henri on 08.05.17.
  */
 @Provided
 public class SalesView implements View {
+
+    private JPanel salespanel;
+    private JTable Bestellungen;
+    private JTable table2;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JScrollPane BestellungenScroll;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField5;
+    private JSpinner spinner1;
+    private JButton hinzufügenButton;
+    private JTable table1;
+    private JTextField textField6;
+    private JTextField textField8;
+    private JTextField textField7;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTable table3;
+    private JTextField textField14;
+    private JButton abbrechenButton;
+    private JButton speichernButton;
+
     @Override
     public void show() {
 
