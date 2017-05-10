11516bcc0ec1d977daf62a2b7b804efeac15d884
Henri Schmidt <henri@reschmi.de>
2017-05-10 14:51:09 +0200

    add JDBC Connector and dependency

---
+++ src/main/java/de/randomerror/persistence/JDBC/Config.java
@@ -0,0 +1,13 @@
+package de.randomerror.persistence.JDBC;
+
+/**
+ * Created by henri on 10.05.17.
+ */
+public class Config {
+    public static final String
+            URL = "",
+            DATABASE = "",
+            USER = "",
+            PASSWORD = "";
+
+}
---
+++ src/main/java/de/randomerror/persistence/JDBC/JDBCConnector.java
@@ -0,0 +1,31 @@
+package de.randomerror.persistence.JDBC;
+
+import lombok.Getter;
+
+import java.sql.*;
+import java.util.LinkedList;
+
+/**
+ * Created by henri on 10.05.17.
+ */
+public class JDBCConnector {
+
+    @Getter
+    private Connection connection;
+
+    public JDBCConnector() {
+        connect();
+    }
+
+    /**
+     * Connects to the database with the data provided from the configuration class {@link Config}
+     */
+    public void connect() {
+        try {
+            // initiate the connection with the database
+            connection = DriverManager.getConnection(Config.URL + Config.DATABASE, Config.USER, Config.PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
