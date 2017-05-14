46dc57ee0caf95153d9e8942a990ddb271ea96af
jwennrich <janwe1508@gmail.com>
2017-05-14 21:22:58 +0200

    removed unimplemented stuff

--- src/main/java/de/randomerror/GUI/controller/LoginViewController.java
+++ src/main/java/de/randomerror/GUI/controller/LoginViewController.java
@@ -1,19 +1,9 @@
 package de.randomerror.GUI.controller;
 
-import de.randomerror.GUI.exceptions.CredentialsInvalidException;
 import de.randomerror.GUI.view.SalesView;
-import de.randomerror.GUI.view.SupplyView;
 import de.randomerror.GUI.view.WarehouseView;
-import de.randomerror.entity.Role;
-import de.randomerror.entity.User;
-import de.randomerror.entity.Warehouse;
-import de.randomerror.persistence.UserRepo;
 import de.randomerror.services.LoginService;
 import de.randomerror.util.Provided;
-import lombok.Getter;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * Created by henri on 08.05.17.
@@ -22,7 +12,7 @@ import java.awt.event.ActionListener;
 public class LoginViewController {
     public WarehouseView warehouseView;
     public SalesView salesView;
-    public SupplyView supplyView;
+
     public LoginService loginService;
 
     public void login(String user, String pass) {
@@ -32,7 +22,7 @@ public class LoginViewController {
     public void transitionToNextView() {
         switch (loginService.getCurrentUser().getRole()) {
             case SUPPLY_HUMAN:
-                supplyView.show();
+//                supplyView.show();
                 break;
             case SALES_HUMAN:
                 salesView.show();
--- src/main/java/de/randomerror/GUI/view/NewOrderView.java
+++
@@ -1,7 +0,0 @@
-package de.randomerror.GUI.view;
-
-/**
- * Created by henri on 08.05.17.
- */
-public class NewOrderView {
-}
