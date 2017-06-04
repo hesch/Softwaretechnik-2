c02f4b5295affc77e80eb40e090405ab02513713
Henri Schmidt <henri@reschmi.de>
2017-06-04 20:06:10 +0200

    add stub for service tests

---
+++ src/test/java/de/randomerror/services/LoginServiceTest.java
@@ -0,0 +1,15 @@
+package de.randomerror.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Created by henri on 04.06.17.
+ */
+public class LoginServiceTest {
+    @Test
+    void pleaseWriteTestHere()  {
+        fail("not yet implemented!");
+    }
+}
---
+++ src/test/java/de/randomerror/services/SalesServiceTest.java
@@ -0,0 +1,16 @@
+package de.randomerror.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Created by henri on 04.06.17.
+ */
+public class SalesServiceTest {
+
+    @Test
+    void pleaseWriteTestHere()  {
+        fail("not yet implemented!");
+    }
+}
