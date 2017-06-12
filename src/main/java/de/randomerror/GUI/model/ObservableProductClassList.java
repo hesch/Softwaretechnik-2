2e982281b949bbb42e5cf600a6321e9042699012
Henri Schmidt <henri@reschmi.de>
2017-06-12 16:50:15 +0200

    add ObservableProductClassList

--- src/main/java/de/randomerror/GUI/model/ObservableProductClassList.java
+++ src/main/java/de/randomerror/GUI/model/ObservableProductClassList.java
@@ -3,10 +3,12 @@ package de.randomerror.GUI.model;
 import de.randomerror.entity.ProductClassDTO;
 import de.randomerror.persistence.DAO.ProductClassDAO;
 import de.randomerror.util.Injector;
+import de.randomerror.util.Provided;
 
 /**
  * Created by henri on 12.06.17.
  */
+@Provided
 public class ObservableProductClassList extends ObservableDataList<ProductClassDTO, ProductClassDAO> {
     @Override
     public void onInit() {
