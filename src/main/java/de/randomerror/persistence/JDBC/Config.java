4234eeee6e4054bc22754ceb2bdaf09d122970ef
Henri Schmidt <henri@reschmi.de>
2017-06-12 22:31:59 +0200

    add theoretical mysql config(comments); does not work in reality

--- src/main/java/de/randomerror/persistence/JDBC/Config.java
+++ src/main/java/de/randomerror/persistence/JDBC/Config.java
@@ -5,9 +5,9 @@ package de.randomerror.persistence.JDBC;
  */
 public class Config {
     public static final String
-            URL = "jdbc:sqlite:yukonDB.sqlite",
-            DATABASE = "",
-            USER = "",
+            URL = "jdbc:sqlite:yukonDB.sqlite", // "jdbc:mysql://localhost/",
+            DATABASE = "", // "yukon",
+            USER = "", // "root",
             PASSWORD = "";
 
 }
