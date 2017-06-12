e0c2e577117876c8ac803eb0b21c80a78ab5c8d1
Henri Schmidt <henri@reschmi.de>
2017-06-12 16:36:14 +0200

    add ObservableDataList as a abstract class to handle similar ObservableLists

---
+++ src/main/java/de/randomerror/GUI/model/ObservableDataList.java
@@ -0,0 +1,55 @@
+package de.randomerror.GUI.model;
+
+import de.randomerror.entity.AbstractEntity;
+import de.randomerror.entity.OrderDTO;
+import de.randomerror.persistence.DAO.DAO;
+import de.randomerror.persistence.DAO.OrderDAO;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Observable;
+
+/**
+ * Created by henri on 12.06.17.
+ */
+public abstract class ObservableDataList<DATA extends AbstractEntity, REPO extends DAO<DATA>> extends Observable {
+    public REPO repo;
+    private List<DATA> data;
+
+    public void addElement(DATA element) {
+        data.add(element);
+        setChanged();
+        notifyObservers(new ObservableEvent(element, EventState.ADDED));
+    }
+
+    public void removeElement(OrderDTO element) {
+        data.remove(element);
+        setChanged();
+        notifyObservers(new ObservableEvent(element, EventState.DELETED));
+    }
+
+    public void removeElement(long id) {
+        data.stream().filter(o -> o.getId() == id).findFirst().ifPresent(o -> {
+            data.remove(o);
+            setChanged();
+            notifyObservers(new ObservableEvent(o, EventState.DELETED));
+        });
+    }
+
+    public void updateElement(DATA element) {
+        data.stream().filter(o -> element.getId() == o.getId()).findFirst().ifPresent(o -> {
+            data.remove(o);
+            data.add(element);
+            setChanged();
+            notifyObservers(new ObservableEvent(element, EventState.MODIFIED));
+        });
+    }
+
+    public List<DATA> getData() {
+        return Collections.unmodifiableList(data);
+    }
+
+    public void onInit() {
+        data = repo.findAll();
+    }
+}
--- src/main/java/de/randomerror/GUI/model/ObservableOrderList.java
+++ src/main/java/de/randomerror/GUI/model/ObservableOrderList.java
@@ -2,56 +2,21 @@ package de.randomerror.GUI.model;
 
 import de.randomerror.entity.OrderDTO;
 import de.randomerror.persistence.DAO.OrderDAO;
+import de.randomerror.util.Injector;
 import de.randomerror.util.Provided;
 import lombok.Getter;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Observable;
+import java.util.*;
 
 /**
  * Created by henri on 12.06.17.
  */
 @Provided
-public class ObservableOrderList extends Observable {
-    public OrderDAO orderRepo;
-    private List<OrderDTO> data;
-
-    public void addElement(OrderDTO order) {
-        data.add(order);
-        setChanged();
-        notifyObservers(new ObservableEvent(order, EventState.ADDED));
-    }
-
-    public void removeElement(OrderDTO order) {
-        data.remove(order);
-        setChanged();
-        notifyObservers(new ObservableEvent(order, EventState.DELETED));
-    }
-
-    public void removeElement(long id) {
-        data.stream().filter(o -> o.getId() == id).findFirst().ifPresent(o -> {
-            data.remove(o);
-            setChanged();
-            notifyObservers(new ObservableEvent(o, EventState.DELETED));
-        });
-    }
-
-    public void updateElement(OrderDTO order) {
-        data.stream().filter(o -> order.getId() == o.getId()).findFirst().ifPresent(o -> {
-            data.remove(o);
-            data.add(order);
-            setChanged();
-            notifyObservers(new ObservableEvent(order, EventState.MODIFIED));
-        });
-    }
-
-    public List<OrderDTO> getData() {
-        return Collections.unmodifiableList(data);
-    }
-
+public class ObservableOrderList extends ObservableDataList<OrderDTO, OrderDAO> {
+    @Override
     public void onInit() {
-        data = orderRepo.findAll();
+        Injector i = Injector.getInstance();
+        repo = i.getProvided(OrderDAO.class);
+        super.onInit();
     }
 }
