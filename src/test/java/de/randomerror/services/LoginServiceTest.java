0db7666f50eba9c5abd4548871630d191be2ebf5
Henri Schmidt <henri@reschmi.de>
2017-06-12 20:44:49 +0200

    fix loginservicetest

--- src/test/java/de/randomerror/services/LoginServiceTest.java
+++ src/test/java/de/randomerror/services/LoginServiceTest.java
@@ -1,7 +1,12 @@
 package de.randomerror.services;
 
 import de.randomerror.entity.Role;
+import de.randomerror.persistence.AddressRepo;
+import de.randomerror.util.Injector;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -10,7 +15,15 @@ import static org.junit.jupiter.api.Assertions.fail;
  * Created by henri on 04.06.17.
  */
 public class LoginServiceTest {
-    LoginService ls;
+    static LoginService ls;
+
+    @BeforeAll
+    static void setup() throws IOException {
+        //wire all the dependencies together
+        Injector.getInstance().init();
+        ls = Injector.getInstance().getProvided(LoginService.class);
+    }
+
     @Test
     void login() {
         ls.login("sales","test");
