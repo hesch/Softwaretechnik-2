924c40ace4b8aa12870653c372dd88ace4a1fe4d
Henri Schmidt <henri@reschmi.de>
2017-07-10 21:12:12 +0200

    extract jdbc config into properties

--- src/main/java/de/randomerror/persistence/JDBC/Config.java
+++ src/main/java/de/randomerror/persistence/JDBC/Config.java
@@ -1,13 +1,44 @@
 package de.randomerror.persistence.JDBC;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  *
  */
 public class Config {
-    public static final String
-            URL = "jdbc:sqlite:yukonDB.sqlite", // "jdbc:mysql://localhost/",
-            DATABASE = "", // "yukon",
-            USER = "", // "root",
-            PASSWORD = "";
+    private static Config instance = new Config();
+    public static Config getInstance() {
+        return instance;
+    }
 
+    private Config() {
+        properties = new Properties();
+        try {
+            InputStream in = ClassLoader.getSystemResourceAsStream("jdbc.properties");
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Properties properties;
+
+
+    public String getUrl() {
+        return properties.getProperty("jdbc.url");
+    }
+
+    public String getDatabase() {
+        return properties.getProperty("jdbc.database");
+    }
+
+    public String getUser() {
+        return properties.getProperty("jdbc.user");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("jdbc.password");
+    }
 }
